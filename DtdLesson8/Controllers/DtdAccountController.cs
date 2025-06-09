d3a66d5f0e1a2dee531a1b04d0b563ad81b8592b
Hiuebran1 <dodung26102005@gmail.com>
2025-06-09 10:25:02 +0700

    first commit

---
+++ DtdLesson8/Controllers/DtdAccountController.cs
@@ -0,0 +1,105 @@
+﻿using DtdLesson8.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace DtdLesson8.Controllers
+{
+    public class DtdAccountController : Controller
+    {
+        // GET: DtdAccountController
+        public ActionResult Index()
+        {
+            List<DtdAccount> accounts = new List<DtdAccount>();
+
+            return View(accounts);
+        }
+
+        // GET: DtdAccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DtdAccountController/Create
+        public ActionResult Create()
+        {
+            DtdAccount model = new DtdAccount();
+            return View(model);
+        }
+
+        // POST: DtdAccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DtdAccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DtdAccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DtdAccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DtdAccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        [AcceptVerbs("Get", "Post")]
+        public ActionResult VerifyPhone(string dtdPhone)
+        {
+            // Simulate a phone number check (e.g., check against a database)
+            // For demonstration, let's assume the phone number is valid if it starts with "+1"
+            Regex _isPhone = new Regex(@"^\+1\d{10}$");
+            if (!_isPhone.IsMatch(dtdPhone))
+            {
+                return Json($"Phone number is not valid,Ex: 0376963462 or 0376.963.462"); // Phone number is valid
+            }
+            else
+            {
+                return Json(true);
+            }
+        }
+    }
+}
---
+++ DtdLesson8/Models/DtdAccount.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DtdLesson8.Models
+{
+    public class DtdAccount
+    {
+        public int ID { get; set; }
+        [
+        Display(Name = "DtdAccount"),
+        Required(ErrorMessage = "Please enter your full name"),
+        MinLength(3, ErrorMessage = "Full name must be at least 3 characters long"),
+        MaxLength(50, ErrorMessage = "Full name must not exceed 50 characters"),
+        ]
+
+
+        public string DtdFullname { get; set; }
+        [Display(Name = "DtdEmail")]
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [DataType(DataType.EmailAddress)]
+        public string DtdEmail { get; set; }
+        [Display(Name = "DtdPhone")]
+        [DataType(DataType.PhoneNumber)]
+        [Remote(action: "CheckPhone", controller: "DtdAccount")]
+
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string DtdPhone { get; set; }
+        [Display(Name = "DtdAddress")]
+        [Required(ErrorMessage = "Please enter your address")]
+        [StringLength(100, ErrorMessage = "Address must not exceed 100 characters")]
+
+        public string DtdAddress { get; set; }
+        [Display(Name = "DtdAvatar")]
+        public string DtdAvatar { get; set; }
+        [Display(Name = "DtdBirthday")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Please enter your date of birth")]
+        public DateTime DtdBirthday { get; set; }
+        [Display(Name = "DtdGender")]
+        public string DtdGender { get; set; }
+        [Display(Name = "DtdPassword")]
+        [DataType(DataType.Password)]
+        public string DtdPassword { get; set; }
+        [Display(Name = "DtdLink")]
+        [Required(ErrorMessage = "Please enter your social media link")]
+        [Url(ErrorMessage = "Please enter a valid URL")]
+        public string DtdLink { get; set; }
+    }
+    
+}
