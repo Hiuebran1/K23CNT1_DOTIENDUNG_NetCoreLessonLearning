f4be877fb335e46b1fe46342ef3078e36ef3e26c
Hiuebran1 <dodung26102005@gmail.com>
2025-06-17 08:15:56 +0700

    first commit

---
+++ Dtdlesson9/Program.cs
@@ -0,0 +1,31 @@
+using Dtdlesson9.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DtdBookStore");
+builder.Services.AddDbContext<DtdBookStoreContext>(x => x.UseSqlServer(connectionString));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=DtdHome}/{action=DtdIndex}/{dtdid?}");
+
+app.Run();
